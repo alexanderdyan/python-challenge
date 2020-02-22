aba9d20668219b3dc29b856bbf41eda4c335a9db
alexanderdyan <58492089+alexanderdyan@users.noreply.github.com>
2020-02-21 23:13:55 -0800

    created a script to count the number of months

---
+++ PyBank/PyBank.py
@@ -0,0 +1,26 @@
+import csv
+import os
+
+#file is in the same folder as our script
+csvPath = 'budget_data.csv'
+
+with open(csvPath) as csvFile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvFile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    numberMonths = 0
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row[0])
+        numberMonths = numberMonths +1
+
+    
+    print(f"Number of Months: {numberMonths}")
+
