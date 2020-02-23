f621de65257e88366ea961ab8060dc8f760dba62
alexanderdyan <58492089+alexanderdyan@users.noreply.github.com>
2020-02-22 23:32:53 -0800

    added greatest increase and decrease in profit/losses over the entire period

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -17,37 +17,47 @@ with open(csvPath) as csvFile:
     numberMonths = 0
     totalProfitLosses = 0
     averageChange = 0
-    currentProfitLoss = 0
     prevProfitLoss = 0
     greatestProfit = 0
+    greatestProfitMonth = ""
     greatestLoss = 0
+    greatestLossMonth = ""
 
     # Read each row of data after the header
     for row in csvreader:
+        #save values of current row
         rowDate = row[0]
         rowProfitLoss = int(row[1])
-        print(rowDate)
+        #count number of months, every row is a new one
         numberMonths += 1
+        #accumulate profit losses for every new row
         totalProfitLosses += rowProfitLoss
+        #Start calculating Changes after 1st month to get differences
         if numberMonths > 1:
             change = rowProfitLoss - prevProfitLoss
+            #accumulate average change to be divided at the end
             averageChange += change
+            #check if change is bigger than recorded profit
+            if change > greatestProfit:
+                greatestProfit = change
+                greatestProfitMonth = rowDate
+            #check if change is smaller than recorded loss
+            if change < greatestLoss:
+                greatestLoss = change
+                greatestLossMonth = rowDate
+        #store profit losses for next row to use
         prevProfitLoss = rowProfitLoss
-
+    
+    #compute the average of sum of changes
     averageChange = averageChange / (numberMonths - 1)
-
-    # # greatest increase in revenue
-    # greatestProfit = max(averageChange)
-    # # greatest decrease in revenue
-    # greatestLoss = min(averageChange)
 
     print("Financial Analysis")
     print("----------------------------")
     print(f"Number of Months: {numberMonths}")
     print(f"Total Profit/Losses: {totalProfitLosses}")
     print(f"Average Change: {averageChange}")
-    print(f"Greatest Profit: {greatestProfit}")
-    print(f"Greatest Loss: {greatestLoss}")
+    print(f"Greatest Increase in Profits:{greatestProfitMonth} (${greatestProfit})")
+    print(f"Greatest Decrease in Profits:{greatestLossMonth} (${greatestLoss})")
 
      
 
