dce6441614bb0cc57b4898e425e4841135e049b3
alexanderdyan <58492089+alexanderdyan@users.noreply.github.com>
2020-02-22 23:14:28 -0800

    added average change

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -16,13 +16,38 @@ with open(csvPath) as csvFile:
 
     numberMonths = 0
     totalProfitLosses = 0
+    averageChange = 0
+    currentProfitLoss = 0
+    prevProfitLoss = 0
+    greatestProfit = 0
+    greatestLoss = 0
 
     # Read each row of data after the header
     for row in csvreader:
-        print(row[0])
+        rowDate = row[0]
+        rowProfitLoss = int(row[1])
+        print(rowDate)
         numberMonths += 1
-        totalProfitLosses += int(row[1])
+        totalProfitLosses += rowProfitLoss
+        if numberMonths > 1:
+            change = rowProfitLoss - prevProfitLoss
+            averageChange += change
+        prevProfitLoss = rowProfitLoss
 
+    averageChange = averageChange / (numberMonths - 1)
 
+    # # greatest increase in revenue
+    # greatestProfit = max(averageChange)
+    # # greatest decrease in revenue
+    # greatestLoss = min(averageChange)
+
+    print("Financial Analysis")
+    print("----------------------------")
     print(f"Number of Months: {numberMonths}")
     print(f"Total Profit/Losses: {totalProfitLosses}")
+    print(f"Average Change: {averageChange}")
+    print(f"Greatest Profit: {greatestProfit}")
+    print(f"Greatest Loss: {greatestLoss}")
+
+     
+
